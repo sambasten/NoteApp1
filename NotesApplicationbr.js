f6f2b284e1718dc44de42a7207fc3d666ec24ced
Sambasten <akinwunmi_s@yahoo.com>
2016-08-17 16:38:49 +0100

    read me edit

--- NotesApplicationbr.js
+++ NotesApplicationbr.js
@@ -1,31 +1,62 @@
 class NotesApplication{
-	let noteList = [];
+	
 	constructor (author) {
 		this.author = author;
+		this.noteList = [];
 	}
 
 /** Fuction to takes note content and 
 * adds to the note list object
 */
 create(note_content) {
-	noteList.push(note_content);
+	  this.noteList.push(note_content);
+	return this.noteList;
 }
 
 /** function to list out notes in noteList
 */
 list(){
-	for (var note_id in noteList){
-		console.log('Note ID: '+ note_id)
-		console.log(noteList[note_id]);
-		console.log('By Author' + this.author)
+	for (let note_id in this.noteList){
+		console.log('Note ID: '+ note_id);
+		console.log(this.noteList[note_id]);
+		console.log('By Author ' + this.author)
 	}
+}
 /**function to get the ID of the note and 
 *return the content of that index as a string
 */
-get(note_id){
-	return noteList[note_id];
+toGet(note_id){
+	if(this.noteList.length === 0){
+		return  'empty'
+	}
+	return this.noteList[note_id];
 }
 
-/**function to 
+/**function to search text
+*/
+search(search_text){
+		for(let note_id in this.noteList){
+			if (this.noteList[note_id].includes(search_text)){
+				console.log('Showing result for search: ' + '['+search_text+']');
+				console.log('Note ID: '+ note_id);
+				console.log(this.noteList[note_id]);
+				console.log('By Author ' + this.author)
+			}
+			return 'Not Found'
+		}
+}
+
+/**function to delete note id
+*/
+toDelete(note_id){
+	this.noteList.splice(note_id);
+	return this.noteList;
+}
+
+/**function to edit note content
+*/
+edit(note_id, new_content){
+	this.noteList[note_id] = new_content
+	return this.noteList;
 }
 }
